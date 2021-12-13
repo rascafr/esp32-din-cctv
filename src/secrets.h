96fda017952999c1d4f31b788a30993028c7ea6f
rascafr <rascafr@gmail.com>
2021-12-13 02:23:11 +0100

    + secret file

---
+++ src/secrets.h
@@ -0,0 +1,17 @@
+#ifndef SECRETS
+#define SECRETS
+
+/*
+ * Telegram API credentials
+ * Find how yo generate your bot token here: https://core.telegram.org/api#bot-api
+ */
+#define BOT_TOKEN "1234567890:ABCDEF"
+#define CHAT_ID "1234567890"
+
+/*
+ * WiFi configuration
+ */
+#define WIFI_SSID "Your Hotspot 📡"
+#define WIFI_PASSWORD "1234567890"
+
+#endif
